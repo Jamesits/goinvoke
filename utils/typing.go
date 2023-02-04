923662c2d5d4d8b5d14ceb366c3825969f7d58cc
James Swineson <github@public.swineson.me>
2023-02-04 10:47:07 +0800

    utils: update doc

--- utils/typing.go
+++ utils/typing.go
@@ -7,6 +7,7 @@ import (
 
 var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9]+`)
 
+// FormatPublicType returns a human-readable Golang public type name from an arbitrary string
 func FormatPublicType(name string) string {
 	name = nonAlphanumericRegex.ReplaceAllString(name, "")
 	name = strings.ToUpper(string(name[0])) + name[1:]
