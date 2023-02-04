218a4be989837c355a7d548ba63c3c9d41235ecf
James Swineson <github@public.swineson.me>
2023-02-04 10:20:19 +0800

    unmarshal: more tests

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -3,9 +3,10 @@
 package goinvoke
 
 import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/jamesits/goinvoke/utils"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/sys/windows"
-	"os"
 	"path/filepath"
 	"testing"
 	"unsafe"
@@ -21,17 +22,25 @@ type user32 struct {
 
 	// tag overrides field name
 	MessageBox *windows.LazyProc `func:"MessageBoxW"`
+
+	// private fields should not be touched
+	gks *windows.LazyProc `func:"GetKeyState"`
 }
 
 func TestUnmarshalUser32(t *testing.T) {
+	var err error
+
 	u := user32{
 		RandomField: 0,
 	}
 	assert.Nil(t, u.MessageBoxA)
 
-	user32Dll := filepath.Join(os.Getenv("WINDIR"), "System32", "user32.dll")
+	sd, err := utils.GetSystemDirectory()
+	assert.NoError(t, err)
 
-	err := Unmarshal(user32Dll, &u)
+	user32Dll := filepath.Join(sd, "user32.dll")
+
+	err = Unmarshal(user32Dll, &u)
 	assert.NoError(t, err)
 
 	// should use insecure search order since we specified an absolute path
@@ -42,16 +51,19 @@ func TestUnmarshalUser32(t *testing.T) {
 	assert.NotNil(t, u.MessageBoxA)
 	assert.NotNil(t, u.MessageBox)
 	assert.NotEqualValues(t, u.MessageBoxA, u.MessageBox)
+
+	assert.Nil(t, u.gks)
 }
 
 // sanity tests
 // some unit tests are from https://github.com/dotnet/pinvoke/blob/c01077e0511e6cec6d860b3373ca2a60ba7cbcae/test/Kernel32.Tests/Kernel32Facts.cs
 type kernel32 struct {
-	GetTickCount   *windows.LazyProc
-	GetTickCount64 *windows.LazyProc
-	SetLastError   *windows.LazyProc
-	SetErrorMode   *windows.LazyProc
-	GetStartupInfo *windows.LazyProc `func:"GetStartupInfoW"`
+	GetTickCount    *windows.LazyProc
+	GetTickCount64  *windows.LazyProc
+	SetLastError    *windows.LazyProc
+	SetErrorMode    *windows.LazyProc
+	GetStartupInfoW *windows.LazyProc
+	GetStartupInfoA *windows.LazyProc
 }
 
 func TestUnmarshalKernel32(t *testing.T) {
@@ -92,14 +104,58 @@ func TestUnmarshalKernel32(t *testing.T) {
 	assert.ErrorIs(t, err, windows.ERROR_SUCCESS)
 
 	// GetStartupInfoW should return something from its reference argument
-	startupInfo := windows.StartupInfo{}
-	ret1, ret2, err = k.GetStartupInfo.Call(uintptr(unsafe.Pointer(&startupInfo)))
+	startupInfoW := windows.StartupInfo{}
+	ret1, ret2, err = k.GetStartupInfoW.Call(uintptr(unsafe.Pointer(&startupInfoW)))
 	assert.NotZero(t, ret1)
 	assert.NotZero(t, ret2)
 	assert.ErrorIs(t, err, windows.ERROR_SUCCESS)
 	// string conversion: https://gist.github.com/NaniteFactory/9e9d3fe5ea7bfeed788b0795162201c7
-	lpTitle := windows.UTF16PtrToString(startupInfo.Title)
-	assert.True(t, len(lpTitle) > 0)
-	lpDesktop := windows.UTF16PtrToString(startupInfo.Desktop)
-	assert.EqualValues(t, "Winsta0\\Default", lpDesktop)
+	lpTitleW := windows.UTF16PtrToString(startupInfoW.Title)
+	assert.True(t, len(lpTitleW) > 0)
+	lpDesktopW := windows.UTF16PtrToString(startupInfoW.Desktop)
+	assert.EqualValues(t, "Winsta0\\Default", lpDesktopW)
+
+	// GetStartupInfoA
+	startupInfoA := windows.StartupInfo{}
+	ret1, ret2, err = k.GetStartupInfoA.Call(uintptr(unsafe.Pointer(&startupInfoA)))
+	assert.NotZero(t, ret1)
+	assert.Zero(t, ret2)
+	assert.ErrorIs(t, err, windows.ERROR_SUCCESS)
+	lpTitleA := utils.UintPtrToString(uintptr(unsafe.Pointer(startupInfoA.Title)))
+	assert.True(t, len(lpTitleA) > 0)
+	lpDesktopA := utils.UintPtrToString(uintptr(unsafe.Pointer(startupInfoA.Desktop)))
+	assert.EqualValues(t, "Winsta0\\Default", lpDesktopA)
+}
+
+func TestFileMissing(t *testing.T) {
+	u := user32{
+		RandomField: 0,
+	}
+
+	user32Dll := "do_not_exist.dll"
+
+	err := Unmarshal(user32Dll, &u)
+	assert.Error(t, err)
+	assert.EqualValues(t, 2, len(err.(*multierror.Error).Errors))
+
+	assert.Zero(t, u.RandomField)
+
+	assert.Nil(t, u.MessageBoxA)
+	assert.Nil(t, u.MessageBox)
+}
+
+func TestProcMissing(t *testing.T) {
+	type fakeUser32 struct {
+		FunctionMissing1 *windows.LazyProc
+		FunctionMissing2 *windows.LazyProc `func:"ArbitraryFakeName"`
+	}
+	u := fakeUser32{}
+	user32Dll := "user32.dll"
+
+	err := Unmarshal(user32Dll, &u)
+	assert.Error(t, err)
+	assert.EqualValues(t, 3, len(err.(*multierror.Error).Errors))
+
+	assert.Nil(t, u.FunctionMissing1)
+	assert.Nil(t, u.FunctionMissing2)
 }
--- utils/strings.go
+++ utils/strings.go
@@ -6,6 +6,9 @@ import (
 )
 
 // UintPtrToString converts a zero-terminated raw "const char *" to a Go string.
+// Example:
+//
+//	var str := utils.UintPtrToString(uintptr(unsafe.Pointer(uint16ptr_variable)))
 func UintPtrToString(ptr uintptr) string {
 	// https://groups.google.com/g/golang-nuts/c/H77hcVt3AAI
 	return C.GoString((*C.char)(unsafe.Pointer(ptr)))
