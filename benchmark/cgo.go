27109ac044997543da1734df983f3366fb2f7454
James Swineson <github@public.swineson.me>
2023-02-04 09:20:00 +0800

    benchmark: initial implementation

---
+++ benchmark/cgo.go
@@ -0,0 +1,13 @@
+package benchmark
+
+//#cgo CFLAGS: -I"C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um"
+//#cgo LDFLAGS:
+
+/*
+#include <debugapi.h>
+*/
+import "C"
+
+func IsDebuggerPresent() int {
+	return int(C.IsDebuggerPresent())
+}
---
+++ benchmark/performance_test.go
@@ -0,0 +1,42 @@
+package benchmark
+
+import (
+	"github.com/jamesits/goinvoke"
+	"golang.org/x/sys/windows"
+	"testing"
+)
+
+type kernel32 struct {
+	IsDebuggerPresent *windows.LazyProc
+}
+
+var isDebuggerPresent uintptr
+
+func BenchmarkSyscallIsDebuggerPresent(b *testing.B) {
+	var ret1 uintptr
+	var err error
+
+	k := kernel32{}
+	kernel32Dll := "kernel32.dll" // should only search for system paths (secure mode)
+
+	err = goinvoke.Unmarshal(kernel32Dll, &k)
+	if err != nil {
+		b.Fail()
+	}
+
+	for n := 0; n < b.N; n++ {
+		ret1, _, err = k.IsDebuggerPresent.Call()
+	}
+
+	isDebuggerPresent = ret1
+}
+
+func BenchmarkCgoIsDebuggerPresent(b *testing.B) {
+	var idp int
+
+	for n := 0; n < b.N; n++ {
+		idp = IsDebuggerPresent()
+	}
+
+	isDebuggerPresent = uintptr(idp)
+}
