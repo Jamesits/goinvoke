2bc508e9445c0dd68571e541fd645a4d8b139026
James Swineson <github@public.swineson.me>
2023-02-14 22:30:09 +0800

    tests: update doc

--- internal/test/manifest.go
+++ internal/test/manifest.go
@@ -2,5 +2,9 @@ package test
 
 // Application manifest generation
 // Works for Goland invoked tests too
-// Installation: https://github.com/akavel/rsrc
+// Installation: see https://github.com/akavel/rsrc
 //go:generate rsrc -manifest app.manifest
+
+// Manifest references:
+// - https://learn.microsoft.com/en-us/windows/win32/sbscs/application-manifests
+// - https://github.com/m13253/chainloader/blob/eb101b244fcb2282495d07269b7a52e94225e7ca/chainloader.exe.manifest
