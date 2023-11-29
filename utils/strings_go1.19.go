ca29273f86954d50ed5c1cfb6871cc0fd4fa8710
James Swineson <github@public.swineson.me>
2023-11-29 22:02:53 +0800

    add unit tests for linux and darwin

---
+++ utils/strings_go1.19.go
@@ -0,0 +1,17 @@
+//go:build !go1.20
+
+package utils
+
+import "C"
+import (
+	"unsafe"
+)
+
+// UintPtrToString converts a zero-terminated raw "const char *" to a Go string.
+// Example:
+//
+//	var str := utils.UintPtrToString(uintptr(unsafe.Pointer(uint16ptr_variable)))
+func UintPtrToString(ptr uintptr) string {
+	// https://groups.google.com/g/golang-nuts/c/H77hcVt3AAI
+	return C.GoString((*C.char)(unsafe.Pointer(ptr)))
+}
