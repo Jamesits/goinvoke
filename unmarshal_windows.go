167fe5c191f00e3292a284723a23d88e862fdf8e
James Swineson <github@public.swineson.me>
2023-11-30 14:26:43 +0800

    fix windows build errors

--- unmarshal_windows.go
+++ unmarshal_windows.go
@@ -1,18 +1,19 @@
-//go:build unix
+//go:build windows
 
 package goinvoke
 
 import (
-	"errors"
 	"github.com/hashicorp/go-multierror"
 	"github.com/jamesits/goinvoke/utils"
+	"golang.org/x/sys/windows"
 	"reflect"
+	"strconv"
 )
 
 // Unmarshal loads the DLL into memory, then fills all struct fields with type *windows.LazyProc with exported functions.
 func Unmarshal(path string, v any) error {
 	var err error
-	var syntheticErr = errors.New("unmarshal failed")
+	var syntheticErr = ErrorUnmarshalFailed
 	var errorOccurred = false
 
 	ld := newLazyDLL(path)
@@ -44,6 +45,7 @@ func Unmarshal(path string, v any) error {
 		}
 
 		if utils.CompatibleType(valueField, typeOfLazyProc) {
+			// LazyProc only supports loading by name
 			proc := ld.NewProc(procName)
 			// try to load the proc now
 			err = proc.Find()
@@ -55,7 +57,15 @@ func Unmarshal(path string, v any) error {
 
 			utils.Set(valueField, proc)
 		} else if utils.CompatibleType(valueField, typeOfProc) {
-			proc, err := d.FindProc(procName)
+			// Windows specific: ordinal
+			ordinal, ordinalParsingError := strconv.ParseInt(utils.GetStructTag(typeField, "ordinal"), 10, 64)
+
+			var proc *windows.Proc
+			if ordinalParsingError == nil { // we have a valid ordinal
+				proc, err = d.FindProcByOrdinal(uintptr(ordinal))
+			} else { // fallback to matching by name
+				proc, err = d.FindProc(procName)
+			}
 			if err != nil {
 				errorOccurred = true
 				syntheticErr = multierror.Append(syntheticErr, err)
